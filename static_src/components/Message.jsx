7c1fa40d88be849ebeeb6d223fca06c6a5ec42a1
Dmitriy Kalashnikov <kelmik91@me.com>
2020-12-10 19:10:08 +0300

    update Message.jsx

--- static_src/components/Message.jsx
+++ static_src/components/Message.jsx
@@ -5,36 +5,33 @@ export default class Message extends React.Component {
         console.log("child did mount");
     }
 
-    Peremeeee = (message) => {
-
+    rendMessage = (message) => {
         const name = message.message.name;
         const text = message.message.text;
-        console.log(message.message.name);
+        
         if (message.message.name == 'Robot') {
             return (
-            <>
-                <span>{text} :</span>
-                <b>{name}</b>
-            </>
+                <>
+                    <span>{text} :</span>
+                    <b>{name}</b>
+                </>
             )
         } else {
             return (
-            <>
-                <b>{name}</b>
-                <span>: {text}</span>
-            </>
+                <>
+                    <b>{name}</b>
+                    <span>: {text}</span>
+                </>
             )
         }
     }
 
     render() {
-        return this.props.messages.map(messages => 
+        return this.props.messages.map(messages =>
             <div key={messages.id} className={messages.name == 'Robot' ? 'divRobot' : ''}>
-                <this.Peremeeee message={messages}/>
-                {/* <b>{messages.name}</b>
-                <span>: {messages.text}</span> */}
+                <this.rendMessage message={messages} />
             </div>
-            )
+        )
             ;
     }
 }
