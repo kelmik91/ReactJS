4b121b2310dcb5947c46ee4a89145f1cc56acbb7
Kelmik91 <kelmik91@me.com>
2020-12-17 01:38:31 +0300

    update Header

--- static_src/components/Header.jsx
+++ static_src/components/Header.jsx
@@ -2,29 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 export default class Header extends React.Component {
     render() {
         return <Grid
-                    container
-                    spacing={4}
-                    direction="row"
-                    justify="space-evenly"
-                    alignItems="center" 
-                >
+            container
+            spacing={4}
+            direction="row"
+            justify="space-evenly"
+            alignItems="center"
+        >
             <Grid item>
-            <Link to='/'>
-            <ListItem button>
-                <h3>Курс по ReactJS</h3>
-                </ListItem>
-            </Link>
+                <Link to='/'>
+                    <ListItem button>
+                        <ListItemText style={{ textAlign: 'center' }} primary='Курс по ReactJS' />
+                    </ListItem>
+                </Link>
             </Grid>
             <Grid item>
-            <Link to='/profile/'>
-            <ListItem button>
-                <h4>Profile</h4>
-                </ListItem>
-            </Link>
+                <Link to='/profile/'>
+                    <ListItem button>
+                        <ListItemText style={{ textAlign: 'center' }} primary='Profile' />
+                    </ListItem>
+                </Link>
             </Grid>
         </Grid>
     }
