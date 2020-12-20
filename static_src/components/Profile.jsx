36b9bb0e408394a58150fb9d5102390c40345bb9
Dmitriy Kalashnikov <kelmik91@me.com>
2020-12-20 18:51:29 +0300

    Home work lesson-5

--- static_src/components/Profile.jsx
+++ static_src/components/Profile.jsx
@@ -6,9 +6,11 @@ import { profile } from './../actions/profileActions.js'
 class Profile extends React.Component {
     render() {
         return <>
-            <div>{this.props.profile.name}</div>
-            {console.log(this.props)}
             <h1>Profile</h1>
+            <div>You name: <b>{this.props.profile.name}</b></div>
+            <div>
+                {this.props.profile.desc}
+            </div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci asperiores veniam error exercitationem quibusdam quam pariatur molestiae mollitia! Quasi laboriosam aspernatur nihil veniam debitis deserunt sit quis aut qui dolorem! Quas recusandae fugiat mollitia blanditiis officiis esse quia nisi aspernatur excepturi placeat ratione accusantium libero sequi, iure magni, nihil soluta.</p>
             <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi iusto neque nisi natus consequuntur dolorem facere in fugiat assumenda, aut distinctio laboriosam saepe ex fuga voluptates ad possimus expedita suscipit quia at quam harum blanditiis. Excepturi, aliquid corporis. Neque et, pariatur omnis molestiae fugit iusto repellendus fuga deserunt voluptatum recusandae quia nostrum, maxime quam veniam non expedita officia. Impedit doloremque iusto modi veritatis natus aliquam nesciunt soluta fugit, inventore, labore illum enim eligendi corporis culpa. Ipsam a voluptates assumenda illum ipsa eos adipisci incidunt quis quos non, harum aperiam dignissimos explicabo quo excepturi sit fugit tenetur similique. Officiis, optio quos.</span>
         </>
@@ -19,6 +21,6 @@ const mapStateToProps = ({ profileReducer }) => ({
     profile: profileReducer.profile,
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({ profile }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
